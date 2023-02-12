791830dd26c74e250a60bd4002d8eb6ec4aa4abd
Oleh Stasiuk <119261825+Oleh-Stasiuk@users.noreply.github.com>
2023-02-12 12:57:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// Завдання 1:
+let city= 'Київ';
+city = 'Львів';
+console.log(city)
+let address = 'city';
+alert (address);
+
+// Завдання 2:
+let n = 10;
+let result = n**3;
+console.log(result);
+
+// Завдання 3:
+let pen = 4;
+let pricePen = 5.25;
+let pencil = 6;
+let pricePencil = 3.5;
+let multiplication = pen * pricePen;
+let multiplication1 = pencil * pricePencil;
+document.write(multiplication + multiplication1);
